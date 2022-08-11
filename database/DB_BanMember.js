d561afdb35166d78ae01bf1b1e33f4f2fac799da
Jesse Thomas <56457691+jessethomascs@users.noreply.github.com>
2022-08-11 02:22:03 -0400

    Added ban and remove features

--- database/DB_BanMember.js
+++ database/DB_BanMember.js
@@ -1,10 +1,28 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const config = require('../config.json');
+const dbconnect = mysql.createConnection(config.mysql);
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('banmember')
-		.setDescription('Bans a student id from database'),
+		.setDescription('Adds user to database if banned from club')
+		.addNumberOption(option =>
+			option.setName('int')
+				.setDescription('Member student ID')
+				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		dbconnect.connect(err => {
+			if (err) return console.log(err);
+			// else
+			console.log('Connection successful for DB_BanMember.js');
+		});
+
+		dbconnect.query('INSERT INTO ' + config.running_tables.Banned_Members + ' VALUES (' + interaction.options.getNumber('int') + ')', (err) => {
+			if (err) console.log(err);
+		});
+		await interaction.reply({ content: 'Query executed.', ephemeral: true });
 	},
 };
+
--- database/DB_RemoveMember.js
+++ database/DB_RemoveMember.js
@@ -1,10 +1,28 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const config = require('../config.json');
+const dbconnect = mysql.createConnection(config.mysql);
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('removemember')
-		.setDescription('Removes a student id from database'),
+		.setDescription('Removes user from database')
+		.addNumberOption(option =>
+			option.setName('int')
+				.setDescription('Member student ID')
+				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		dbconnect.connect(err => {
+			if (err) return console.log(err);
+			// else
+			console.log('Connection successful for DB_RemoveMember.js');
+		});
+
+		dbconnect.query('DELETE FROM ' + config.running_tables.Registered_Members + ' WHERE StudentID = ' + interaction.options.getNumber('int'), (err) => {
+			if (err) console.log(err);
+		});
+		await interaction.reply({ content: 'Query executed.', ephemeral: true });
 	},
 };
+
